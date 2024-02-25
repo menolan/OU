59012334f6d92b24e89b8152b6c96541e398e8cd
Matt Nolan <menolan22@gmail.com>
2024-02-25 12:58:50 -0500

    adding completed pending confirmation

--- WorkOrderAPI/updateStatus.js
+++ WorkOrderAPI/updateStatus.js
@@ -14,7 +14,7 @@ const updateStatus = async (workOrderId, accessToken) => {
       {
         Status: {
           Primary: "COMPLETED",
-          Extended: "",
+          Extended: "PENDING CONFIRMATION",
         },
       },
       { headers }
